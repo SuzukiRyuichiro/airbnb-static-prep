c5519adafa026e3fe4ffcb5c7ffa960e8339e5f1
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2023-08-14 16:34:59 +0100

    index done

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,3 +1,11 @@
+require 'open-uri'
+require 'json'
 class FlatsController < ApplicationController
-  def index; end
+  def index
+    # Make a request to API
+    # Parse the JSON
+    # assign the array to an instance variable
+    json = URI.open('https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/flats.json').read
+    @flats = JSON.parse(json)
+  end
 end
--- test/system/flats_test.rb
+++ test/system/flats_test.rb
@@ -1,9 +1,15 @@
-require "application_system_test_case"
+require 'application_system_test_case'
 
 class FlatsTest < ApplicationSystemTestCase
-  test "visiting home page" do
+  test 'visiting home page should print out title' do
     visit '/'
 
-    assert_selector "h1", text: "Flats"
+    assert_selector 'h1', text: 'Flats'
+  end
+
+  test 'visiting home page should show list of flats' do
+    visit '/'
+
+    assert_selector 'ul li'
   end
 end
